753c47f45002e448e547eb6378808d605ae8d75d
veragrosskop <veragrosskop@gmail.com>
2026-05-26 10:04:25 +0200

    initial commit

---
+++ flights_data.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, text
+
+QUERY_FLIGHT_BY_ID = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.ID = :id"
+
+# Define the database URL
+DATABASE_URL = "sqlite:///data/flights.sqlite3"
+
+# Create the engine
+engine = create_engine(DATABASE_URL)
+
+
+def execute_query(query, params):
+    """
+    Execute an SQL query with the params provided in a dictionary,
+    and returns a list of records (dictionary-like objects).
+    If an exception was raised, print the error, and return an empty list.
+    """
+    try:
+        with engine.connect() as conn:
+            # your code here
+            pass
+    except Exception as e:
+        print("Query error:", e)
+        return []
+
+
+def get_flight_by_id(flight_id):
+    """
+    Searches for flight details using flight ID.
+    If the flight was found, returns a list with a single record.
+    """
+    params = {'id': flight_id}
+    return execute_query(QUERY_FLIGHT_BY_ID, params)
+
